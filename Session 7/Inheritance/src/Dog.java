3d5633a0c4d52a802d55f242469c91e622ec8909
KULDEEP <kuldeepdhangarkd@gmail.com>
2023-05-28 21:36:38 +0530

    Complete Inheritance - Part-2 (Overridden method)

--- Session 7/Inheritance/src/Dog.java
+++ Session 7/Inheritance/src/Dog.java
@@ -1,5 +1,38 @@
 public class Dog extends Animal {
+    private String earShape;
+    private String tailShape;
+
     public Dog(){
         super("Mutt", "Big", 50);
+    }
+
+    public Dog(String type, double weight){
+        this(type, weight, "Perky", "Curled");
+    }
+
+    @Override
+    public void move(String speed) {
+        super.move(speed);
+        System.out.println("Dogs walk, run and wag their tail");
+    }
+
+    public Dog(String type, double weight, String earShape, String tailShape) {
+        super(type, weight < 15 ? "small": (weight < 35 ? "medium": "large"), weight);
+        this.earShape = earShape;
+        this.tailShape = tailShape;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "earShape='" + earShape + '\'' +
+                ", tailShape='" + tailShape + '\'' +
+                "} " + super.toString();
+    }
+
+
+    // Concept of Overriding methode
+    public void makeNoise(){
+        super.makeNoise();
     }
 }
--- Session 7/Inheritance/src/Main.java
+++ Session 7/Inheritance/src/Main.java
@@ -2,8 +2,15 @@ public class Main {
     public static void main(String[] args){
         Animal animal = new Animal("Generic Animal", "Huge", 40);
         doAnimalStuff(animal, "slow");
+
         Dog dog = new Dog();
         doAnimalStuff(dog, "fast");
+
+        Dog yorkie = new Dog("yorkie", 15);
+        doAnimalStuff(yorkie, "fast");
+
+        Dog retriever = new Dog("Labrador Retriever", 65, "Floppy", "Swimmer");
+        doAnimalStuff(retriever, "slow");
     }
     public static void doAnimalStuff(Animal animal, String speed){
         animal.makeNoise();
