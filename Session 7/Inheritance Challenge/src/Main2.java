f1340c7c451719131d838364bfc65d27c20c45a1
KULDEEP <kuldeepdhangarkd@gmail.com>
2023-05-29 19:44:43 +0530

    changes

--- Session 7/Inheritance Challenge/src/Main2.java
+++ Session 7/Inheritance Challenge/src/Main2.java
@@ -1,13 +1,13 @@
 public class Main2 {
     public static void main(String[] args) {
         Employee tim = new Employee("Tim", "11/11/1985", "01/01/2020");
-        infoEmplo(tim);
-        Employee tim1 = new Employee("Tim1", "11/11/1985", "01/01/2020");
-        infoEmplo(tim1);
+        doEmployee(tim);
+        Employee jimmy = new Employee("Jimmy", "01/01/1999", "03/03/2020");
+        doEmployee(jimmy);
     }
-    public static void infoEmplo(Employee emplo){
-        System.out.println(emplo);
-        System.out.println("Age = " + emplo.getAge());
-        System.out.println("Pay = " + emplo.collectPay());
+    public static void doEmployee(Employee employee){
+        System.out.println(employee);
+        System.out.println("Age = " + employee.getAge());
+        System.out.println("Pay = " + employee.collectPay());
     }
 }
