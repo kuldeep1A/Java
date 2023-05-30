5476079014630304ec4f008e703fb7fa0332547b
KULDEEP <kuldeepdhangarkd@gmail.com>
2023-05-30 17:49:38 +0530

    this vs super 'example'

---
+++ Session 7/this vs super/src/Rectangle.java
@@ -0,0 +1,20 @@
+public class Rectangle {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    public Rectangle(){
+        this(0, 0); // calls 2nd constructor
+    }
+    public Rectangle(int width, int height){
+        this(0, 0, width, height); // calls 3rd constructor
+    }
+
+    public Rectangle(int x, int y, int width, int height) {
+        // Initialize variables
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+}
