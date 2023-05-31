b60e9c0448959e643bf7fd031107062301016cd6
KULDEEP <kuldeepdhangarkd@gmail.com>
2023-05-31 19:52:11 +0530

    complete composition part-1

--- Section 8/Composition/src/Product.java
+++ Section 8/Composition/src/Product.java
@@ -12,19 +12,56 @@ public class Product {
 }
 
 class Monitor extends Product {
+    private int size;
+    private String resolution;
+
     public Monitor(String model, String manufacturer) {
         super(model, manufacturer);
+    }
+
+    public Monitor(String model, String manufacturer, int size, String resolution) {
+        super(model, manufacturer);
+        this.size = size;
+        this.resolution = resolution;
+    }
+
+    public void drawPixelAt(int x, int y, String color){
+        System.out.printf("Drawing pixel at %d, %d in color %s ", x, y, color);
     }
 }
 
 class Motherboard extends Product {
+    private int ramSlots;
+    private int cardSlots;
+    private String bios;
+
     public Motherboard(String model, String manufacturer){
         super(model, manufacturer);
+    }
+
+    public Motherboard(String model, String manufacturer, int ramSlots, int cardSlots, String bios) {
+        super(model, manufacturer);
+        this.ramSlots = ramSlots;
+        this.cardSlots = cardSlots;
+        this.bios = bios;
+    }
+
+    public void loadProgram(String programName){
+        System.out.println("Program " + programName + " is now loading...");
     }
 }
 
 class ComputerCase extends Product {
+    private String powerSupply;
     public ComputerCase(String model, String manufacturer){
         super(model, manufacturer);
+    }
+
+    public ComputerCase(String model, String manufacturer, String powerSupply){
+        super(model, manufacturer);
+        this.powerSupply = powerSupply;
+    }
+    public void pressPowerButton(){
+        System.out.println("Power button pressed");
     }
 }
