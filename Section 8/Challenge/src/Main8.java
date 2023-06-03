50d62418c63d592d7300f2ba6cceea1dfd2df928
KULDEEP <kuldeepdhangarkd@gmail.com>
2023-06-03 21:25:59 +0530

    Composition 'Challenge'

--- Section 8/Challenge/src/Main8.java
+++ Section 8/Challenge/src/Main8.java
@@ -2,14 +2,14 @@ public class Main8 {
     public static void main(String[] args) {
         SmartKitchen kitchen = new SmartKitchen();
 
-        kitchen.getBrewMaster().setHasWorkToDo(true);
-        kitchen.getIceBox().setHasWorkTodo(true);
-        kitchen.getDishWasher().setHasWorkToDo(true);
-//        kitchen.setKitchenState(true, true, true);
+//        kitchen.getBrewMaster().setHasWorkToDo(true);
+//        kitchen.getIceBox().setHasWorkTodo(true);
+//        kitchen.getDishWasher().setHasWorkToDo(true);
 
-        kitchen.getDishWasher().doDishes();
-        kitchen.getIceBox().orderFood();
-        kitchen.getBrewMaster().brewCoffee();
-//        kitchen.doKitchenWork();
+//        kitchen.getDishWasher().doDishes();
+//        kitchen.getIceBox().orderFood();
+//        kitchen.getBrewMaster().brewCoffee();
+        kitchen.setKitchenState(true, false, true);
+        kitchen.doKitchenWork();
     }
 }
