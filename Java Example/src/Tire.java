2b57720b2e95b7be78f1d3f6c6f717aabb53a932
KULDEEP <kuldeepdhangarkd@gmail.com>
2023-06-04 21:05:44 +0530

    solve 'countNode' countUniqueSubstring

--- Java Example/src/Tire.java
+++ Java Example/src/Tire.java
@@ -71,22 +71,55 @@ public class Tire {
         return true;
     }
 
+    public static int countNode(Node root){
+        if (root == null){
+            return 0;
+        }
+        int count = 0;
+        for(int i = 0; i < 26; i++){
+            if(root.children[i] != null){
+                count += countNode(root.children[i]);
+            }
+        }
+        return count + 1;
+    }
+
     public static void main(String[] args) {
 //        String[] words = {"the", "a", "three", "their", "any"};
 //        insert(words);
 //        String[] keys = {"the", "an", "three", "their", "any"};
 //        search(keys);
-        String[] words1 = {"i", "like", "sam", "samsung", "mobile", "ice"};
-        insert(words1);
+//        String[] words1 = {"i", "like", "sam", "samsung", "mobile", "ice"};
+//        insert(words1);
 //        for(String word: words){
 //            System.out.println(word + " : " + search(word));
 //        }
 
-        String key = "ilikesamsung";
-        System.out.println(wordBreak(key));
-        String[] words = {"apple", "app", "mango", "man", "woman"};
-        insert(words);
-        String prefix = "app";
-        System.out.println( prefix + " : " + startWith(prefix));
+//        String key = "ilikesamsung";
+//        System.out.println(wordBreak(key));
+//        String[] words = {"apple", "app", "mango", "man", "woman"};
+//        insert(words);
+//        String prefix = "app";
+//        System.out.println( prefix + " : " + startWith(prefix));
+
+
+
+        // Substring = all unique prefix of all suffix
+        // total numbers of tire == count of unique prefix
+        String str = "ababa";
+        String[] suffix = new String[str.length() + 1];
+        // find suffix of 'str'
+        for (int i = 0; i < str.length() + 1; i++){
+            suffix[i] = str.substring(i);
+            if (i == str.length()){
+                suffix[i] = "";
+            }
+        }
+        // print all suffix
+//        for (String s: suffix){
+//            System.out.println(s);
+//        }
+        insert(suffix);
+        System.out.println(countNode(root));
     }
 }
