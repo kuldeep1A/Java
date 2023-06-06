b97e437c0e1d7b70a99fa6e6c1e5efd98dbaaf30
KULDEEP <kuldeepdhangarkd@gmail.com>
2023-06-06 17:42:22 +0530

    added weight 'Graph'

--- Java Example/src/Graph.java
+++ Java Example/src/Graph.java
@@ -4,31 +4,43 @@ public class Graph {
     static class Edge{
         int src;
         int dest;
+        int wet;
 
-        public Edge(int src, int dest){
+        public Edge(int src, int dest, int wet){
             this.src = src;
             this.dest = dest;
+            this.wet = wet;
         }
+    }
+
+    public static void addEdge(ArrayList<Edge>[] graph, int src, int dest, int wet){
+        graph[src].add(new Edge(src, dest, wet));
     }
 
     public static void createGraph(ArrayList<Edge>[] graph){
         for(int i = 0; i < graph.length; i++){
             graph[i] = new ArrayList<Edge>();
         }
-        graph[0].add(new Edge(0, 2));
-        graph[1].add(new Edge(1, 2));
-        graph[1].add(new Edge(1, 3));
-        graph[2].add(new Edge(2, 0));
-        graph[2].add(new Edge(2, 1));
-        graph[2].add(new Edge(2, 3));
-        graph[3].add(new Edge(3, 1));
-        graph[3].add(new Edge(3, 2));
+
+        addEdge(graph, 0, 2, 2);
+        addEdge(graph, 1, 2, 10);
+        addEdge(graph, 1, 3, 0);
+        addEdge(graph, 2, 0, 2);
+        addEdge(graph, 2, 1, 10);
+        addEdge(graph, 2, 3, -1);
+        addEdge(graph, 3, 1, 0);
+        addEdge(graph, 3, 2, -1);
     }
 
     public static void main(String[] args) {
         int V = 4;
         ArrayList<Edge>[] graph = new ArrayList[V];
+        createGraph(graph);
 
-
+        // print 2's neighbour
+        for(int i = 0; i < graph[2].size(); i++){
+            Edge e = graph[2].get(i);
+            System.out.println(e.dest + ", " + e.wet);
+        }
     }
 }
