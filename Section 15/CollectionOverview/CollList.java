21a95896257957cd1250843abb542a47fc7336ad
KULDEEP <kuldeepdhangarkd@gmail.com>
2023-06-10 19:29:52 +0530

    'changes' CollectionOverview

---
+++ Section 15/CollectionOverview/CollList.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CollList {
+    public static void main(String[] args) {
+//        List<String> list = new ArrayList<>();
+//        String[] names = {"Anna", "Bob", "Carol", "David", "Edna"};
+//        list.addAll(Arrays.asList(names));
+        String[] names = {"Anna", "Bob", "Carol", "David", "Edna"};
+        List<String> list = new ArrayList<>(Arrays.asList(names));
+        list.add("naveenBro");
+        list.addAll(Arrays.asList("George", "Gray", "Grace"));
+        System.out.println(list);
+        System.out.println("Harry is in the list " + list.contains("Harry"));
+
+        list.removeIf(s -> s.charAt(0) == 'A');
+        System.out.println(list); // Remove Anna because at index of string 0 is 'A'
+
+    }
+}
