94e68e7359b034e86a243bedf914f8e22be2da85
KULDEEP <kuldeepdhangarkd@gmail.com>
2023-06-10 13:21:58 +0530

    type

---
+++ Java Example/leetcode/maxValue.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class maxValue {
+    public static int maxValue1(int n, int index, int maxSum){
+        int[] nums = new int[n];
+        Arrays.fill(nums, 0);
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+
+        }
+    }
+    public static void main(String[] args) {
+        int n = 4;
+        int index = 2;
+        int maxSum = 6;
+        System.out.println(maxValue1(n, index, maxSum));
+    }
+}
