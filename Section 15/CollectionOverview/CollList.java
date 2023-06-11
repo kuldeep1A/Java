2ccc04ae38dded83caaa9f14d785bae54dfa4cfe
KULDEEP <kuldeepdhangarkd@gmail.com>
2023-06-11 20:14:18 +0530

    'changes' CollectionOverview

--- Section 15/CollectionOverview/CollList.java
+++ Section 15/CollectionOverview/CollList.java
@@ -9,13 +9,15 @@ public class CollList {
         List<String> list = new ArrayList<>(Arrays.asList(names));
         list.add("naveenBro");
         list.addAll(Arrays.asList("George", "Gray", "Grace"));
-        System.out.println(list);
+        System.out.println("Array list: " + list);
         System.out.println("Harry is in the list " + list.contains("Harry"));
 
         list.removeIf(s -> s.charAt(0) == 'A');
-        System.out.println(list); // Remove Anna because at index of string 0 is 'A'
-        Collection<String> treeSet = new TreeSet<>();
-        treeSet.addAll(Arrays.asList(names));
-        System.out.println(treeSet);
+        System.out.println("Array list: " + list); // Remove Anna because at index of string 0 is 'A'
+        Collection<String> treeSet = new TreeSet<>(Arrays.asList(names));
+        System.out.println("TreeSet: " + treeSet);
+        Collection<String> hashSet = new HashSet<>(Arrays.asList(names));
+
+        System.out.println("HashSet: " + hashSet);
     }
 }
