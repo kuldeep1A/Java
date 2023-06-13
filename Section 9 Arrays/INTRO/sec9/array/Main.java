c041934eccb359d5c8867c9b61c44b283dcfaecc
KULDEEP <kuldeepdhangarkd@gmail.com>
2023-06-13 23:13:09 +0530

    'Arrays Part 1'

--- Section 9 Arrays/INTRO/sec9/array/Main.java
+++ Section 9 Arrays/INTRO/sec9/array/Main.java
@@ -1,46 +1,36 @@
 package sec9.array;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-public class Main {
-    public static List<Integer> divideArrayIntoSubsets(int[] arr) {
-        Arrays.sort(arr);
-        reverseArray(arr);
-
-        List<Integer> subsetA = new ArrayList<>();
-        int sumA = 0;
-        int sumB = 0;
-
-        for (int num : arr) {
-            if (sumA <= sumB) {
-                subsetA.add(num);
-                sumA += num;
-            } else {
-                sumB += num;
-            }
-        }
-
-        return subsetA;
-    }
-
-    // Helper method to reverse the array in place
-    private static void reverseArray(int[] arr) {
-        int start = 0;
-        int end = arr.length - 1;
-        while(start < end){
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            start++;
-            end--;
-        }
-    }
-
+public class Main{
     public static void main(String[] args) {
-        int[] arr = {3,7,5,6,2};
-        List<Integer> subsetA = divideArrayIntoSubsets(arr);
-        System.out.println("Subset A: " + subsetA);
+        int[] myIntArray = new int[10];
+//        Generate the error
+//        myIntArray[0] = 46.0;
+//        myIntArray[1] = "1";
+        myIntArray[0] = 45;
+        myIntArray[1] = 1;
+        myIntArray[5] = 50;
+        System.out.println(myIntArray[5]);
+
+        // The array initializer
+        int[] firstFivePositives = new int[]{1,2,3,4,5};
+        System.out.println(firstFivePositives[3]);
+
+
+        double[] myDobuleArray = new double[10];
+        myDobuleArray[2] = 3.5;
+        System.out.println(myDobuleArray[2]);
+
+        String[] names = {"Andy", "Bob", "Charlie", "David", "Eve"};
+        int[] firstTen = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println("First: " + firstTen[0]);
+        int arrayLength = firstTen.length;
+        System.out.println("Length of array = " + arrayLength);
+        System.out.println("Last  = " + firstTen[arrayLength - 1]);
+
+        int[] newArray;
+        newArray = new int[] {5, 4, 3, 2, 1};
+        for(int i: newArray){
+            System.out.print(i + " ");
+        }
     }
 }
