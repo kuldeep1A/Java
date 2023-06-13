df49ea0fbfd7683ba426da0e852ed8341e6308fd
KULDEEP <kuldeepdhangarkd@gmail.com>
2023-06-13 19:04:00 +0530

    solve 'isPalindrome'

---
+++ Java Example/Algo/String or Array/src/isPalindrome.java
@@ -0,0 +1,23 @@
+class isPalindrome {
+    public static boolean isPalindrome1(String s) {
+        int l = 0;
+        int r = s.length() - 1;
+
+        while(l < r){
+            while (l < r && !Character.isLetterOrDigit(s.charAt(l)))
+                ++l;
+            while (l < r && !Character.isLetterOrDigit(s.charAt(r)))
+                --r;
+            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r)))
+                return false;
+            ++l;
+            --r;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String str = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome1(str));
+
+    }
+}
