9b72fa5239b644c5f1a9aa22f6eabab274b6a9db
KULDEEP <kuldeepdhangarkd@gmail.com>
2023-06-13 23:35:00 +0530

    'Arrays Part 2'

--- Section 9 Arrays/INTRO/sec9/array/Main.java
+++ Section 9 Arrays/INTRO/sec9/array/Main.java
@@ -1,5 +1,8 @@
 package sec9.array;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Main{
     public static void main(String[] args) {
         int[] myIntArray = new int[10];
@@ -12,7 +15,7 @@ public class Main{
         System.out.println(myIntArray[5]);
 
         // The array initializer
-        int[] firstFivePositives = new int[]{1,2,3,4,5};
+        int[] firstFivePositives = new int[]{1, 2, 3, 4, 5};
         System.out.println(firstFivePositives[3]);
 
 
@@ -28,9 +31,40 @@ public class Main{
         System.out.println("Last  = " + firstTen[arrayLength - 1]);
 
         int[] newArray;
-        newArray = new int[] {5, 4, 3, 2, 1};
-        for(int i: newArray){
-            System.out.print(i + " ");
+        // Array initialization and default element values are 0
+        newArray = new int[5];
+        for (int i = 0; i < newArray.length; i++) {
+            newArray[i] = newArray.length - i;
+            System.out.print(newArray[i] + " ");
         }
+        // Using Enhanced for loop, the for each loop
+        // for (declaration: collection){
+        //  ``````// block of statements
+        // }
+        System.out.println();
+        for(int element: newArray){
+            System.out.print(element + " ");
+        }
+        System.out.println();
+        System.out.println(Arrays.toString(newArray));
+        Object objectVariable = newArray;
+        if (objectVariable instanceof int[]){
+            System.out.println("objectVariable is really an int array");
+        }
+        Object[] objectArray = new Object[4];
+        objectArray[0] = "Hello";
+        objectArray[1] = 2;
+        objectArray[2] = 0.3;
+        objectArray[3] = new StringBuilder("Word");
+
+
+        for(Object d : objectArray){
+            if (d instanceof StringBuilder){
+                System.out.println("In this index of array have a String Builder like: " + d);
+            } else{
+                System.out.println("In this index of array  does not have string object");
+            }
+        }
+
     }
 }
