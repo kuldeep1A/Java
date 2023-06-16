85c49ee6674b7b0ba679c7bd76a59324d98483d4
KULDEEP <kuldeepdhangarkd@gmail.com>
2023-06-17 00:02:01 +0530

    solve pathSumIII

---
+++ Java Example/Algo/Tree or Binary Search Tree/src/pathSumIII.java
@@ -0,0 +1,35 @@
+package src;
+
+public class pathSumIII extends OwnBTree{
+    public pathSumIII(int data){
+        super(data);
+    }
+
+    public static int _pathSumIII(OwnBTree tree, int target){
+        if (tree == null)
+            return 0;
+        return dfs(tree, target) + _pathSumIII(tree.left, target) + _pathSumIII(tree.right, target);
+    }
+    private static int dfs(OwnBTree tree, int target){
+        if (tree == null)
+            return 0;
+
+        return (target == tree.data ? 1 : 0) + dfs(tree.left, target - tree.data)
+                + dfs(tree.right, target - tree.data);
+    }
+    public static void main(String[] args) {
+        pathSumIII tree = new pathSumIII(10);
+        tree.left = new OwnBTree(5);
+        tree.left.left = new OwnBTree(3);
+        tree.left.left.left = new OwnBTree(3);
+        tree.left.left.right = new OwnBTree(-2);
+        tree.left.right = new OwnBTree(2);
+        tree.left.right.right = new OwnBTree(1);
+        tree.right = new OwnBTree(-3);
+        tree.right.right = new OwnBTree(11);
+
+        int ans = _pathSumIII(tree, 8);
+        System.out.println(ans);
+
+    }
+}
