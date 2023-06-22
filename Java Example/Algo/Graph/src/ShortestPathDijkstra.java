5bf947763ec595e2baa9573ff42c29086df94f7d
KULDEEP <kuldeepdhangarkd@gmail.com>
2023-06-22 17:36:37 +0530

    solve the Shortest Path of each node using Dijkstra

--- Java Example/Algo/Graph/src/ShortestPathDijkstra.java
+++ Java Example/Algo/Graph/src/ShortestPathDijkstra.java
@@ -1,5 +1,6 @@
 package src;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.PriorityQueue;
 
@@ -18,35 +19,81 @@ public class ShortestPathDijkstra extends Graph {
 
         @Override
         public int compareTo(Pair other){
-            return Integer.compare(this.dest, other.dest);
+            return this.dest - other.dest;
         }
     }
 
-    public static void Dijkstra(Graph graph, int source){
-        Pair node = new Pair(0, 0);
-        PriorityQueue<Pair> pair = new PriorityQueue<>();
-        pair.add(node);
+    public static int[] Dijkstra(Graph graph, int parent){
+        PriorityQueue<Pair> pairPQ = new PriorityQueue<>();
+        int[] distance = new int[graph.getNumVertices()];
+        boolean[] visited = new boolean[graph.getNumVertices()];
+        for (int i = 0; i < graph.getNumVertices(); i++){
+            if (i != parent){
+                distance[i] = Integer.MAX_VALUE;
+            }
+        }
+
+        // first source (parent)  distance is always zero
+        pairPQ.add(new Pair(0, 0));
+
+        while (!pairPQ.isEmpty()){
+            Pair current = pairPQ.remove();
+            if (!visited[current.node]){
+                visited[current.node] = true;
+                List<Edge> neighbors = graph.getAdjacencyList().get(current.node);
+                for (Edge neighbor: neighbors){
+                    int source = neighbor.source;    // u
+                    int destination = neighbor.destination;   // v
+                    int weight = neighbor.weight;    //w
+
+                    // performs relaxation
+                    if (distance[source] + weight < distance[destination]){
+                        distance[destination] = distance[source] + weight;
+                        pairPQ.add(new Pair(destination, distance[destination]));
+                    }
+                }
+            }
+        }
+        return distance;
     }
     public static void main(String[] args) {
         ShortestPathDijkstra graph = new ShortestPathDijkstra(9);
-        graph.addEdgeUW(0, 1, 4);
-        graph.addEdgeUW(0, 7, 8);
-        graph.addEdgeUW(1, 2, 8);
-        graph.addEdgeUW(1, 7, 11);
-        graph.addEdgeUW(2, 3, 7);
-        graph.addEdgeUW(2, 5, 4);
-        graph.addEdgeUW(2, 8, 2);
-        graph.addEdgeUW(3, 4, 9);
-        graph.addEdgeUW(3, 5, 14);
-        graph.addEdgeUW(4, 5, 10);
-        graph.addEdgeUW(5, 6, 2);
-        graph.addEdgeUW(6, 8, 6);
-        graph.addEdgeUW(6, 7, 1);
-        graph.addEdgeUW(7, 8, 7);
+//        https://www.geeksforgeeks.org/dijkstras-shortest-path-algorithm-greedy-algo-7/
+        graph.addEdgeUSW(0, 1, 4);
+        graph.addEdgeUSW(0, 7, 8);
+        graph.addEdgeUSW(1, 2, 8);
+        graph.addEdgeUSW(1, 7, 11);
+        graph.addEdgeUSW(2, 3, 7);
+        graph.addEdgeUSW(2, 5, 4);
+        graph.addEdgeUSW(2, 8, 2);
+        graph.addEdgeUSW(3, 4, 9);
+        graph.addEdgeUSW(3, 5, 14);
+        graph.addEdgeUSW(4, 5, 10);
+        graph.addEdgeUSW(5, 6, 2);
+        graph.addEdgeUSW(6, 8, 6);
+        graph.addEdgeUSW(6, 7, 1);
+        graph.addEdgeUSW(7, 8, 7);
+
+        ShortestPathDijkstra graph2 = new ShortestPathDijkstra(6);
+        graph2.addEdgeSW(0, 1, 2);
+        graph2.addEdgeSW(0, 2, 4);
+        graph2.addEdgeSW(1, 2, 1);
+        graph2.addEdgeSW(1, 3, 7);
+        graph2.addEdgeSW(2, 4, 3);
+        graph2.addEdgeSW(3, 5, 1);
+        graph2.addEdgeSW(4, 3, 2);
+        graph2.addEdgeSW(4, 5, 5);
 
         graph.printGraph();
 
-//        List<Integer> res = Dijkstra(graph, 0);
-//        System.out.println(res);
+
+        int[] res = Dijkstra(graph, 0);
+        System.out.println("graph1: " + Arrays.toString(res));
+        System.out.println("\n\n");
+
+        graph2.printGraph();
+
+        res = Dijkstra(graph2,0);
+        System.out.println("graph2: " + Arrays.toString(res));
     }
 }
