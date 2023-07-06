95c19d337d16dfa89b7210add821baecb7249b9c
KULDEEP <kuldeepdhangarkd@gmail.com>
2023-07-06 23:27:31 +0530

    use of Object

--- Section 9 Arrays/Multiple Arrays/multiple/Main.java
+++ Section 9 Arrays/Multiple Arrays/multiple/Main.java
@@ -31,7 +31,21 @@ public class Main {
 
         Object[] anyArray = new Object[3];
         System.out.println(Arrays.toString(anyArray));
+        System.out.println("-".repeat(20));
         anyArray[0] = new String[] {"a", "b", "c"};
+        System.out.println(Arrays.deepToString(anyArray));
+        System.out.println("-".repeat(20));
+        anyArray[1] = new String[][] {{"1", "2"},{"3", "4", "5"},{"6", "7", "8", "9"}};
+        System.out.println(Arrays.deepToString(anyArray));
+        System.out.println("-".repeat(20));
+        anyArray[2] = new int[2][2][2];
+        System.out.println(Arrays.deepToString(anyArray));
+        System.out.println("-".repeat(20));
 
+        for(Object element: anyArray){
+            System.out.println("Element type = " + element.getClass().getSimpleName());
+            System.out.println("Element toString() = " + element);
+            System.out.println(Arrays.deepToString((Object[]) element));
+        }
     }
 }
