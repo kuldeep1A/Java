7536ca12d7001d8534b244174804930d49e0f178
KULDEEP <kuldeepdhangarkd@gmail.com>
2023-07-07 15:18:38 +0530

    complete part 2

--- Section 10 Lists, ArrayLists & LinkedLists/List and ArrayList Part1/dev/Main.java
+++ Section 10 Lists, ArrayLists & LinkedLists/List and ArrayList Part1/dev/Main.java
@@ -7,6 +7,11 @@ record GroceryItem(String name, String type, int count) {
     public GroceryItem(String name){
         this(name, "DAIRY", 1);
     }
+
+    @Override
+    public String toString() {
+        return String.format("%d %s in %s", count, name.toUpperCase(), type);
+    }
 }
 public class Main {
     public static void main(String[] args) {
@@ -15,13 +20,32 @@ public class Main {
         groceryArray[1] = new GroceryItem("apple", "PRODUCE", 6);
         groceryArray[2] = new GroceryItem("oranges", "PRODUCE", 2);
         System.out.println(Arrays.toString(groceryArray));
+        System.out.println("-".repeat(20));
 
         ArrayList objectList = new ArrayList();
-        objectList.add(new GroceryItem("Butter"));
+        objectList.add(new GroceryItem("Banana"));
         objectList.add("Mango");
+        System.out.println(objectList);
+        System.out.println("-".repeat(20));
 
         ArrayList<GroceryItem> groceryList = new ArrayList<>();
         groceryList.add(new GroceryItem("Butter"));
+        groceryList.add(new GroceryItem("Milk"));
+        groceryList.add(new GroceryItem("Oranges", "PRODUCE", 5));
+
+        // does not remove the first index to increase the growth of arrayList
+        groceryList.add(0, new GroceryItem("Apple", "PRODUCE", 12));
+        System.out.println(groceryList);
+
+        System.out.println("-".repeat(20));
+
+        // when we use set methode instead of add to remove the index and declare new value of that index
+        groceryList.set(0, new GroceryItem("Banana", "PRODUCE", 120));
+        System.out.println(groceryList);
+
+        // remove the third item in the list
+        System.out.println("remove-".repeat(5));
+        groceryList.remove(2);
         System.out.println(groceryList);
     }
 }
